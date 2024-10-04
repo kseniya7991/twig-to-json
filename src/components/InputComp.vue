ac376488605c842af06c60bcc4dace48cb2e5b79
kseniya7991 <kseniya7991@gmail.com>
2024-10-04 10:10:32 +0300

    clear

--- src/components/InputComp.vue
+++ src/components/InputComp.vue
@@ -17,6 +17,26 @@ export default {
     const resultObj = ref({});
 
     const startConvert = () => {
+      // Пример простого форматирования
+
+      // console.log("Available languages:");
+      // console.log(JSON.stringify(Object.keys(Prism.languages)));
+
+      // console.log(
+      //   Prism.highlight(textarea.value, Prism.languages.twig, "twig")
+      // );
+
+      // codeBlock.value.innerHTML = Prism.highlight(
+      //   textarea.value,
+      //   Prism.languages.twig,
+      //   "twig"
+      // );
+
+      // textarea.value = textarea.value
+      //   .replace(/>\s+</g, ">\n<") // добавляет переносы строк между тегами
+      //   .replace(/^\s+/gm, "") // удаляет начальные пробелы в строке
+      //   .replace(/\s+$/gm, ""); // удаляет конечные пробелы в строке
+
       resultObj.value = {};
       const combinedRegex =
         /(?<for>{%\s*for\b.*?%})|(?<endfor>{%\s*endfor\s*%})|(?<={{)(?<content>\w+(\.\w+)*)(?=}})/gm;
@@ -40,7 +60,7 @@ export default {
           ({ level, lastFor, lastObjStack } = handleFor(
             el,
             level,
-            lastObjStack
+            lastObjStack,
           ));
         } else if (el.groups.endfor) {
           level--;
